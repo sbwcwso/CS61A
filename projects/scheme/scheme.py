dcdd25bf120c02d9c0b18ad537ea058b368a4609
sbwcwso <lijunjie199502@gmail.com>
2023-07-19 11:05:22 +0800

    WIP from emacs: /home/lijunjie/Documents/Course/cs61a/projects/scheme/scheme.py

--- projects/scheme/scheme.py
+++ projects/scheme/scheme.py
@@ -160,7 +160,7 @@ class BuiltinProcedure(Procedure):
         # Convert a Scheme list to a Python list
         python_args = []
         # BEGIN PROBLEM 3
-        
+        arg = args.
         # END PROBLEM 3
         try:
             return self.fn(*python_args)
