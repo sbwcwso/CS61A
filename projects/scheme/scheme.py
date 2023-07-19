0f1b6a94e3fa4bd107f8895ff224000f6875569d
sbwcwso <lijunjie199502@gmail.com>
2023-07-19 10:41:08 +0800

    WIP from emacs: /home/lijunjie/Documents/Course/cs61a/projects/scheme/scheme.py

--- projects/scheme/scheme.py
+++ projects/scheme/scheme.py
@@ -98,7 +98,7 @@ class Frame(object):
     def lookup(self, symbol):
         """Return the value bound to SYMBOL. Errors if SYMBOL is not found."""
         # BEGIN PROBLEM 2
-        "*** YOUR CODE HERE ***"
+        
         # END PROBLEM 2
         raise SchemeError('unknown identifier: {0}'.format(symbol))
 
