160e6f75825c7e95e19e432b3badae5338c97cd0
sbwcwso <lijunjie199502@gmail.com>
2023-07-19 10:21:46 +0800

    WIP from emacs: /home/lijunjie/Documents/Course/cs61a/projects/scheme/scheme_reader.py

--- projects/scheme/scheme_reader.py
+++ projects/scheme/scheme_reader.py
@@ -165,7 +165,7 @@ def read_tail(src):
         else:
             # BEGIN PROBLEM 1
             first = scheme_read(src)
-            rest = read_tail()
+            rest = read_tail(src)
             # END PROBLEM 1
     except EOFError:
         raise SyntaxError('unexpected end of file')
