8ef8a9dc9dc283bcc54e280a6c4a4ab78e044a9b
sbwcwso <lijunjie199502@gmail.com>
2023-07-19 11:19:32 +0800

    WIP from emacs: /home/lijunjie/Documents/Course/cs61a/projects/scheme/scheme.py

--- projects/scheme/scheme.py
+++ projects/scheme/scheme.py
@@ -36,7 +36,7 @@ def scheme_eval(expr, env, _=None): # Optional third argument is ignored
         return SPECIAL_FORMS[first](rest, env)
     else:
         # BEGIN PROBLEM 4
-        
+        procedure = 
         # END PROBLEM 4
 
 def self_evaluating(expr):
