97ee9e673d6d3fe1c637edbcdf17bff87253343b
sbwcwso <lijunjie199502@gmail.com>
2023-07-19 10:18:49 +0800

    WIP from emacs: /home/lijunjie/Documents/Course/cs61a/projects/scheme/scheme_reader.py

--- projects/scheme/scheme_reader.py
+++ projects/scheme/scheme_reader.py
@@ -131,7 +131,7 @@ def scheme_read(src):
     val = src.pop_first() # Get and remove the first token
     if val == 'nil':
         # BEGIN PROBLEM 1
-        "*** YOUR CODE HERE ***"
+        
         # END PROBLEM 1
     elif val == '(':
         # BEGIN PROBLEM 1
