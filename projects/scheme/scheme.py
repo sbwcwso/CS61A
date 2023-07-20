9bd3c6d9e3752e09256f6bffb28251a717b92a4e
sbwcwso <lijunjie199502@gmail.com>
2023-07-20 09:57:43 +0800

    WIP from emacs: /home/lijunjie/Documents/Course/cs61a/projects/scheme/scheme.py

--- projects/scheme/scheme.py
+++ projects/scheme/scheme.py
@@ -270,7 +270,7 @@ def do_quote_form(expressions, env):
     """
     validate_form(expressions, 1, 1)
     # BEGIN PROBLEM 6
-    return expressions
+    return expressions.first
     # END PROBLEM 6
 
 def do_begin_form(expressions, env):
