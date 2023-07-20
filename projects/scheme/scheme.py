d4de630f7fb04f0ea7707b6151742956b1411235
sbwcwso <lijunjie199502@gmail.com>
2023-07-20 09:02:45 +0800

    WIP from emacs: /home/lijunjie/Documents/Course/cs61a/projects/scheme/scheme.py

--- projects/scheme/scheme.py
+++ projects/scheme/scheme.py
@@ -36,6 +36,7 @@ def scheme_eval(expr, env, _=None): # Optional third argument is ignored
         return SPECIAL_FORMS[first](rest, env)
     else:
         # BEGIN PROBLEM 4
+        first = schem
         procedure = env.lookup(first)
         validate_procedure(procedure)
         rest = rest.map(lambda item: scheme_eval(item, env))
