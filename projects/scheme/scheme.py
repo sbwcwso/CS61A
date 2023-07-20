2b4f2ccb6cbdfd8b39e7a2acbb8ba4245aab03cf
sbwcwso <lijunjie199502@gmail.com>
2023-07-20 09:29:15 +0800

    WIP from emacs: /home/lijunjie/Documents/Course/cs61a/projects/scheme/scheme.py

--- projects/scheme/scheme.py
+++ projects/scheme/scheme.py
@@ -249,7 +249,9 @@ def do_define_form(expressions, env):
     if scheme_symbolp(target): 
         validate_form(expressions, 2, 2) # Checks that expressions is a list of length exactly 2
         # BEGIN PROBLEM 5
-        variable, value = expressions.first, 
+        variable, value = expressions.first, expressions.rest
+        if isinstance(value, Pair):
+            value = 
         env.define(variable, value)
         # END PROBLEM 5
     elif isinstance(target, Pair) and scheme_symbolp(target.first):
