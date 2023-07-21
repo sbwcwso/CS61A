7f59bcaa764e2f074b40562bb129e9702b5f6b62
sbwcwso <lijunjie199502@gmail.com>
2023-07-21 10:09:01 +0800

    WIP from emacs: /home/lijunjie/Documents/Course/cs61a/projects/scheme/scheme.py

--- projects/scheme/scheme.py
+++ projects/scheme/scheme.py
@@ -397,7 +397,7 @@ def do_cond_form(expressions, env):
             # BEGIN PROBLEM 13
             if clause.rest is nil:
                 return test
-            return 
+            return eval_all
             # END PROBLEM 13
         expressions = expressions.rest
 
