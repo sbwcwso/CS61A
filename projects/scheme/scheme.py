d0becc1db78b0ffe37f2387f8b184250c2a9c920
sbwcwso <lijunjie199502@gmail.com>
2023-07-21 10:34:43 +0800

    WIP from emacs: /home/lijunjie/Documents/Course/cs61a/projects/scheme/scheme.py

--- projects/scheme/scheme.py
+++ projects/scheme/scheme.py
@@ -428,7 +428,7 @@ def make_let_frame(bindings, env):
         value = scheme_eval(binding.rest.first, env)
         values = Pair(value, values)
         bindings = bindings.rest
-    valid
+    validate_formals()
     # END PROBLEM 14
     return env.make_child_frame(names, values)
     
