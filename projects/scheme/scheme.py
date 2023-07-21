2a69a2d00081c44f0f844155ff5b858ac3b59b72
sbwcwso <lijunjie199502@gmail.com>
2023-07-21 09:16:13 +0800

    WIP from emacs: /home/lijunjie/Documents/Course/cs61a/projects/scheme/scheme.py

--- projects/scheme/scheme.py
+++ projects/scheme/scheme.py
@@ -264,7 +264,8 @@ def do_define_form(expressions, env):
     elif isinstance(target, Pair) and scheme_symbolp(target.first):
         # BEGIN PROBLEM 9
         formals = target.rest
-        body = target
+        body = expressions.rest
+        do_la
         # END PROBLEM 9
     else:
         bad_target = target.first if isinstance(target, Pair) else target
