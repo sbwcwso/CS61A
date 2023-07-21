33a73f1b01572b6d766ba03fc50f5f751a599c80
sbwcwso <lijunjie199502@gmail.com>
2023-07-21 10:18:30 +0800

    WIP from emacs: /home/lijunjie/Documents/Course/cs61a/projects/scheme/scheme.py

--- projects/scheme/scheme.py
+++ projects/scheme/scheme.py
@@ -423,7 +423,8 @@ def make_let_frame(bindings, env):
     # BEGIN PROBLEM 14
     while bindings is not nil:
         binding = bindings.first
-        validate_form()
+        validate_form(binding, 2, 2)
+        
     # END PROBLEM 14
     return env.make_child_frame(names, values)
     
