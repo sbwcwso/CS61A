bcebe8a0b333043d9185dfdd0dc66fefca14ece8
sbwcwso <lijunjie199502@gmail.com>
2023-07-21 08:54:44 +0800

    WIP from emacs: /home/lijunjie/Documents/Course/cs61a/projects/scheme/scheme.py

--- projects/scheme/scheme.py
+++ projects/scheme/scheme.py
@@ -74,7 +74,7 @@ def eval_all(expressions, env):
     value = None
     expression = expressions.first
     while expression != nil:
-        value = scheme_ev
+        value = scheme_eval()
     return scheme_eval(expressions.first, env) # change this line
     # END PROBLEM 7
 
