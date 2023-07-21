a4c5eebc5b6e7c2bb0206a6994b3c446450012ac
sbwcwso <lijunjie199502@gmail.com>
2023-07-21 10:17:43 +0800

    WIP from emacs: /home/lijunjie/Documents/Course/cs61a/projects/scheme/scheme.py

--- projects/scheme/scheme.py
+++ projects/scheme/scheme.py
@@ -421,7 +421,8 @@ def make_let_frame(bindings, env):
         raise SchemeError('bad bindings list in let form')
     names, values = nil, nil
     # BEGIN PROBLEM 14
-    
+    while bindings in not nil:
+        
     # END PROBLEM 14
     return env.make_child_frame(names, values)
     
