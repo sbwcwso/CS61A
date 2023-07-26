06232b882422dd61e3f962fc3944efc9fbaf4fdc
sbwcwso <lijunjie199502@gmail.com>
2023-07-26 11:06:50 +0800

    WIP from emacs: /home/lijunjie/Documents/Course/cs61a/projects/scheme/scheme.py

--- projects/scheme/scheme.py
+++ projects/scheme/scheme.py
@@ -73,7 +73,7 @@ def eval_all(expressions, env):
     # BEGIN PROBLEM 7
     res = None
     while expressions != nil:
-        res = scheme_eval(expressions.first, env, tail=True if expressions.rest is )
+        res = scheme_eval(expressions.first, env, tail=True if expressions.rest is nil else False)
         expressions = expressions.rest
     return res
     # END PROBLEM 7
