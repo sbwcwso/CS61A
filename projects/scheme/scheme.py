496eaaee275d71e2a5dc48ef4a488d35c832fce1
sbwcwso <lijunjie199502@gmail.com>
2023-07-27 08:47:49 +0800

    WIP from emacs: /home/lijunjie/Documents/Course/cs61a/projects/scheme/scheme.py

--- projects/scheme/scheme.py
+++ projects/scheme/scheme.py
@@ -219,6 +219,7 @@ class MacroProcedure(LambdaProcedure):
         """Apply this macro to the operand expressions."""
         if len(self.formals) != len(operands):
             raise SchemeError('Incorrect number of operands to marco apply')
+        macro
         return complete_apply(self, operands, env)
 
 def add_builtins(frame, funcs_and_names):
