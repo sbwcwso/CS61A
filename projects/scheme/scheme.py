146b06d01e2d8a3754bdd3d3307794ee0a5a8851
sbwcwso <lijunjie199502@gmail.com>
2023-07-27 09:04:54 +0800

    WIP from emacs: /home/lijunjie/Documents/Course/cs61a/projects/scheme/scheme.py

--- projects/scheme/scheme.py
+++ projects/scheme/scheme.py
@@ -37,6 +37,8 @@ def scheme_eval(expr, env, tail=False): # Optional third argument is ignored
     else:
         # BEGIN PROBLEM 4
         procedure = scheme_eval(first, env)
+        if isinstance(procedure, MacroProcedure):
+            return procedure.apply_macro(args, env)
         validate_procedure(procedure)
         args = rest.map(lambda item: scheme_eval(item, env))
         return scheme_apply(procedure, args, env)
