1ff6a854f9b0198c30c4ac918a41c16de670e7ed
sbwcwso <lijunjie199502@gmail.com>
2023-07-27 08:31:57 +0800

    WIP from emacs: /home/lijunjie/Documents/Course/cs61a/projects/scheme/scheme.py

--- projects/scheme/scheme.py
+++ projects/scheme/scheme.py
@@ -452,7 +452,7 @@ def do_define_macro(expressions, env):
         # BEGIN PROBLEM 9
         formals = target.rest
         body = expressions.rest
-        procedure = MacroProcedure(forma)
+        procedure = MacroProcedure(formals, body, env)
         procedure = do_lambda_form(Pair(formals, body), env)
         env.define(target.first, procedure)
         return target.first 
