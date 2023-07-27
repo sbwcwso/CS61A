3fa4ac59c6a6d4cc06b22379478beb5342afe807
sbwcwso <lijunjie199502@gmail.com>
2023-07-27 08:56:59 +0800

    WIP from emacs: /home/lijunjie/Documents/Course/cs61a/projects/scheme/scheme.py

--- projects/scheme/scheme.py
+++ projects/scheme/scheme.py
@@ -224,7 +224,7 @@ class MacroProcedure(LambdaProcedure):
         while formals != nil:
             macro_env.define(formals.first, operands.first)
             formals, operands = formals.rest, operands.rest
-        self.body = schem
+        self.body = scheme
         return complete_apply(self, operands, env)
 
 def add_builtins(frame, funcs_and_names):
