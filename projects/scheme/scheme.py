6b7b15998819b50c1394e4ce129a4034e6e07fd3
sbwcwso <lijunjie199502@gmail.com>
2023-07-27 09:42:45 +0800

    WIP from emacs: /home/lijunjie/Documents/Course/cs61a/projects/scheme/scheme.py

--- projects/scheme/scheme.py
+++ projects/scheme/scheme.py
@@ -467,7 +467,6 @@ def do_define_macro(expressions, env):
     if  scheme_symbolp(target):
         body = expressions.rest
         validate_formals(formals)
-        validate_type(body, Pair)
         validate_form(body, 1)
         procedure = MacroProcedure(formals, body, env)
         env.define(target, procedure)
