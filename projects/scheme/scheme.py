53c06e5acb81f99a1a2a05f826aae10b9f726af0
sbwcwso <lijunjie199502@gmail.com>
2023-07-27 08:34:33 +0800

    WIP from emacs: /home/lijunjie/Documents/Course/cs61a/projects/scheme/scheme.py

--- projects/scheme/scheme.py
+++ projects/scheme/scheme.py
@@ -440,7 +440,6 @@ def do_define_macro(expressions, env):
     """
     # BEGIN Problem 20
     validate_form(expressions, 2) # Checks that expressions is a list of length at least 2
-    target = expressions.first
     if scheme_symbolp(target.first):
         formals = target.rest if isinstance(target, Pair) else nil
         body = expressions.rest
