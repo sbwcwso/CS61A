3a97a9a727fb71d3b178f89e89b12193ea897ecd
sbwcwso <lijunjie199502@gmail.com>
2023-07-27 09:33:42 +0800

    WIP from emacs: /home/lijunjie/Documents/Course/cs61a/projects/scheme/scheme.py

--- projects/scheme/scheme.py
+++ projects/scheme/scheme.py
@@ -54,7 +54,7 @@ def scheme_apply(procedure, args, env):
     if isinstance(procedure, BuiltinProcedure):
         return procedure.apply(args, env)
     if isinstance(procedure, MacroProcedure):
-        return scheme_
+        return scheme_eval()
     else:
         new_env = procedure.make_call_frame(args, env)
         return eval_all(procedure.body, new_env)
